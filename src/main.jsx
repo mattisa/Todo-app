d0ae90ede30749e4d30769acfd1ca4b2a8ff0cd8
Ibrahim Hiarea <ibrahimhiarea@gmail.com>
2023-10-17 23:52:20 +0300

    Some Changes

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './app/App'
-import { HashRouter  } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux'
 import store from './app/store';
 import './index.css'
@@ -9,9 +9,9 @@ import './index.css'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <HashRouter >
+      <BrowserRouter>
         <App />
-      </HashRouter >
+      </BrowserRouter>
     </Provider>
   </React.StrictMode>,
 )
